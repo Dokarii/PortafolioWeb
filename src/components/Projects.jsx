65d9e18d7965e291afb97ecd116e3ad4c68c0cb7
Dokari <ricardotejedor@gmail.com>
2026-04-28 19:31:51 -0500

    Añadir nuevo proyecto y actualizar favicon

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,12 +7,23 @@ const projectsData = [
     id: 1,
     title: "Generador de Comentarios",
     description:
-      "Geneador de comentarios con la estetica de Twitch, perfectos para streamers y creadores de contenido. \n\nFoto de ilgmyzin en Unsplash.",
+      "Geneador de comentarios con la estetica de Twitch, perfectos para streamers y creadores de contenido. 100% personalizable y facil de usar. Con descarga directa y vista previa en tiempo real. \n\nFoto de ilgmyzin en Unsplash.",
     image:
       "https://images.unsplash.com/photo-1679065236532-09c4a500a025?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     tags: ["HTML5", "JavaScript", "CSS"],
     demoUrl: "https://commentgenerator.vercel.app/",
     repoUrl: "https://github.com/Dokarii/GeneradorComentarios",
+  },
+  {
+    id: 2,
+    title: "Python Fundamentals for Data Science",
+    description:
+      "Repositorio de GitHub con micro-proyectos que forman parte de un roadmap estructurado de autoaprendizaje para la especialización en Ciencia de Datos y Machine Learning. \n\nFoto de Chris Ried en Unsplash.",
+    image:
+      "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    tags: ["Python", "Jupyter", "Data Science", "Machine Learning"],
+    demoUrl: "https://github.com/Dokarii/Python-Fundamentals-for-Data-Science",
+    repoUrl: "https://github.com/Dokarii/Python-Fundamentals-for-Data-Science",
   },
 ];
 
